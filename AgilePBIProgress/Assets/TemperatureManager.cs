d352ea899e887632937a29d47074de8953d218f1
svc-student <svc-student@student.directory.jccc>
2018-09-19 13:44:57 -0500

    Added Temperature Change On Height

---
+++ AgilePBIProgress/Assets/TemperatureManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TemperatureManager : MonoBehaviour
+{
+    public float currentTemperature;
+    public Transform temperatureManager;
+
+    private float baseTemperature;
+
+	// Use this for initialization
+	void Start ()
+    {
+        SetBaseTemperature();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        SetBaseTemperature();
+        SetTemperature();
+	}
+
+    public void SetTemperature()
+    {
+        if (transform.position.y > temperatureManager.position.y)
+        {
+            currentTemperature = baseTemperature - (transform.position.y - temperatureManager.position.y);
+        }
+        else
+        {
+            currentTemperature = baseTemperature;
+        }
+    }
+
+    public void SetBaseTemperature()
+    {
+        baseTemperature = temperatureManager.GetComponent<TemperatureSetter>().baseTemperature;
+    }
+}
