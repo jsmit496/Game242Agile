d4a5dd4491aa9efc06406a3ba8f9f9da38623a1d
jsmit496 <jsmit496@stumail.jccc.edu>
2018-09-26 07:41:07 -0500

    sprint1 Tasks Complete

--- AgilePBIProgress/Assets/TemperatureManager.cs
+++ AgilePBIProgress/Assets/TemperatureManager.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TemperatureManager : MonoBehaviour
 {
-    public float currentTemperature;
+    public int currentTemperature;
     public Transform temperatureManager;
+    public Text temperatureText;
 
-    private float baseTemperature;
+    public GameObject[] temperatureGauges;
+
+    private int baseTemperature;
+    private Transform oldTemperatureManager;
 
 	// Use this for initialization
 	void Start ()
@@ -20,22 +25,45 @@ public class TemperatureManager : MonoBehaviour
     {
         SetBaseTemperature();
         SetTemperature();
+        CheckClosestGauge();
 	}
 
     public void SetTemperature()
     {
         if (transform.position.y > temperatureManager.position.y)
         {
-            currentTemperature = baseTemperature - (transform.position.y - temperatureManager.position.y);
+            currentTemperature = (int)(baseTemperature - (transform.position.y - temperatureManager.position.y));
         }
         else
         {
             currentTemperature = baseTemperature;
         }
+        temperatureText.text = "Temperature: " + currentTemperature + "°F";
     }
 
     public void SetBaseTemperature()
     {
         baseTemperature = temperatureManager.GetComponent<TemperatureSetter>().baseTemperature;
+    }
+
+    public void CheckClosestGauge()
+    {
+        float currentClosestDistance = 9999999999;
+        GameObject currentClosest = temperatureManager.gameObject;
+
+        foreach (GameObject gbj in temperatureGauges)
+        {
+            float distance = Vector3.Distance(transform.position, gbj.transform.position);
+            if (distance < currentClosestDistance)
+            {
+                currentClosestDistance = distance;
+                currentClosest = gbj;
+            }
+        }
+        if (temperatureManager != currentClosest.transform)
+        {
+            temperatureManager = currentClosest.transform;
+            SetBaseTemperature();
+        }
     }
 }
